864d041e7ba042172d9c77edf04a129d1ea0d38a
menna <161839658+MT1152@users.noreply.github.com>
2024-07-20 14:54:28 +0300

    Add files via upload

---
+++ dataalgorithm/build/classes/dataalgorithm/Dataalgorithm.rs
@@ -0,0 +1,10 @@
+dataalgorithm.sortowind$1
+dataalgorithm.SortingVisualization
+dataalgorithm.sortowind
+dataalgorithm.arraywindo
+dataalgorithm.arraywindo$1
+dataalgorithm.SortingVisualization$1
+dataalgorithm.Welco
+dataalgorithm.Welco$1
+dataalgorithm.SortingPanel
+dataalgorithm.Dataalgorithm
